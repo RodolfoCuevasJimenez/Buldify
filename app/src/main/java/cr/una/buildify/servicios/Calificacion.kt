88b89ed6c9272c8ffa6dc63a0a03b3b5f986c045
Sebastián Alfaro Delgado <sebasalfaro1205@gmail.com>
2023-06-10 11:31:43 -0600

    Calificacion y Servicio

---
+++ app/src/main/java/cr/una/buildify/servicios/Calificacion.kt
@@ -0,0 +1,7 @@
+package cr.una.buildify.servicios
+
+data class Calificacion (
+    var idServicio:String ="",
+    var calDirector:Int=-1,
+    var calDuenno:Int=-1
+)
--- app/src/main/java/cr/una/buildify/servicios/Servicio.kt
+++ app/src/main/java/cr/una/buildify/servicios/Servicio.kt
@@ -1,14 +1,16 @@
 package cr.una.buildify.servicios
 
 
-
 data class Servicio(
+        var idUsuario: String = "",
         var tipo: String = "",
         var titulo: String = "",
         var descripcion: String = "",
         var direccion: String = "",
         var nombrePersona: String = "",
         var telefono: String = "",
-        var correoElectronico: String = ""
+        var correoElectronico: String = "",
+        var calificaciones: ArrayList<Calificacion>? = ArrayList(),
+        var calificacionPromedio: Float = 0.0f
 )
 
